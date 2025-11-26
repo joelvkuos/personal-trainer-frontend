f226837a338e8d309caf8c8bd921fa5ce8f1917c
bhr469@myy.haaga-helia.fi <bhr469@myy.haaga-helia.fi>
2025-11-26 15:38:56 +0200

    Navbar additions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,25 @@
-import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <nav>
-            <Link to="/">Customers</Link>
-            <Link to="/trainings">Trainings</Link>
-            <Link to="/calendar">Calendar</Link>
-            <Link to="/statistics">Statistics</Link>
-
-        </nav>
-    )
+        <AppBar position="static">
+            <Toolbar>
+                <Button color="inherit" component={Link} to="/">
+                    Customers
+                </Button>
+                <Button color="inherit" component={Link} to="/trainings">
+                    Trainings
+                </Button>
+                <Button color="inherit" component={Link} to="/calendar">
+                    Calendar
+                </Button>
+                <Button color="inherit" component={Link} to="/statistics">
+                    Statistics
+                </Button>
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 export default Navbar;
