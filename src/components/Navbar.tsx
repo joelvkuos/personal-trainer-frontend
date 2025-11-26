4cc9dba70dfde646e26c2a1bdfd9fe5f0ab550cf
bhr469@myy.haaga-helia.fi <bhr469@myy.haaga-helia.fi>
2025-11-26 15:46:08 +0200

    Navbar color additions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,69 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <AppBar position="static">
+        <AppBar position="static" sx={{ backgroundColor: '#67645E' }}>
             <Toolbar>
-                <Button color="inherit" component={Link} to="/">
+                <Button
+                    variant="outlined"
+                    sx={{
+                        color: '#D1F0FD',
+                        borderColor: '#D1F0FD',
+                        '&:hover': {
+                            borderColor: '#D1F0FD',
+                            backgroundColor: 'rgba(209, 240, 253, 0.1)'
+                        }
+                    }}
+                    component={Link}
+                    to="/"
+                >
                     Customers
                 </Button>
-                <Button color="inherit" component={Link} to="/trainings">
+                <Button
+                    variant="outlined"
+                    sx={{
+                        color: '#D1F0FD',
+                        borderColor: '#D1F0FD',
+                        marginLeft: '1rem',
+                        '&:hover': {
+                            borderColor: '#D1F0FD',
+                            backgroundColor: 'rgba(209, 240, 253, 0.1)'
+                        }
+                    }}
+                    component={Link}
+                    to="/trainings"
+                >
                     Trainings
                 </Button>
-                <Button color="inherit" component={Link} to="/calendar">
+                <Button
+                    variant="outlined"
+                    sx={{
+                        color: '#D1F0FD',
+                        borderColor: '#D1F0FD',
+                        marginLeft: '1rem',
+                        '&:hover': {
+                            borderColor: '#D1F0FD',
+                            backgroundColor: 'rgba(209, 240, 253, 0.1)'
+                        }
+                    }}
+                    component={Link}
+                    to="/calendar"
+                >
                     Calendar
                 </Button>
-                <Button color="inherit" component={Link} to="/statistics">
+                <Button
+                    variant="outlined"
+                    sx={{
+                        color: '#D1F0FD',
+                        borderColor: '#D1F0FD',
+                        marginLeft: '1rem',
+                        '&:hover': {
+                            borderColor: '#D1F0FD',
+                            backgroundColor: 'rgba(209, 240, 253, 0.1)'
+                        }
+                    }}
+                    component={Link}
+                    to="/statistics"
+                >
                     Statistics
                 </Button>
             </Toolbar>
